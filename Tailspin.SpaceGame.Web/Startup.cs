659a5c63d4acb50fee036ba49ea849ac124304bf
Ma Cristina Englis <ma.cristina.englis@accenture.com>
2022-08-16 10:00:29 +0800

    fix pipeline error

--- Tailspin.SpaceGame.Web/Startup.cs
+++ Tailspin.SpaceGame.Web/Startup.cs
@@ -21,6 +21,7 @@ namespace TailSpin.SpaceGame.Web
             Configuration = configuration;
         }
 
+        //Add comment
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
